e669d663111916a27c9629645bb90543dc65bcd3
Immortalbug <stephenvicson@gmail.com>
2021-09-29 11:06:50 +0200

    Done with Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,57 @@
 "use strict";
 let id = 5;
+let developer = 'Stephen';
+let isActive = true;
+let arr = [1, 'string', false];
+let person = developer;
+console.log('ID:', id);
+console.log('Developer name:', person);
+// Union
+let union;
+union: 'Hey union';
+// Enum
+var Direction;
+(function (Direction) {
+    Direction["up"] = "up";
+    Direction["Down"] = "Down";
+    Direction["Right"] = "Right";
+    Direction["Left"] = "Left";
+})(Direction || (Direction = {}));
+console.log(Direction.Left);
+const user = {
+    id: 4,
+    name: 'Morlex'
+};
+console.log(user);
+// Type assertation
+let uid;
+let UserId = uid;
+UserId = '200';
+console.log(UserId);
+// Funnction
+function addNum(x, y) {
+    return x * y;
+}
+console.log(addNum(5, 3));
+const man = {
+    id: 4,
+    name: 'Morlex'
+};
+console.log(man);
+// Classes
+class animal {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+const dog = new animal(1, 'Spike');
+console.log(dog);
+// Generic
+function getArray(items) {
+    return Array().concat(items);
+}
+let numArray = getArray([1, 2, 3, 4, 5, 6]);
+let strArray = getArray(['step', 'joe', 'doe']);
+numArray.splice(3);
+console.log(numArray);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,90 @@
-let id: number = 5;
+let id: number = 5
+let developer: string = 'Stephen'
+let isActive: boolean = true
+
+let arr: any[] = [1, 'string', false]
+
+let person = developer
+console.log('ID:', id)
+console.log('Developer name:', person)
+
+
+// Union
+let union: string | number
+union: 'Hey union'
+
+// Enum
+enum Direction{
+    up= 'up',
+    Down='Down',
+    Right='Right',
+    Left='Left'
+}
+
+console.log(Direction.Left)
+
+
+// Object
+
+type User = {
+    id: number
+    name:string
+}
+const user: User = {
+    id:4,
+    name:'Morlex'
+}
+console.log(user)
+
+// Type assertation
+
+let uid: any
+let UserId = <string>uid
+
+UserId = '200'
+console.log(UserId)
+
+
+// Funnction
+function addNum(x:number, y:number): any {
+    return x*y
+}
+console.log(addNum(5, 3))
+
+
+// Interfaces
+
+interface Person  {
+    id: number
+    name:string
+}
+const man: Person = {
+    id:4,
+    name:'Morlex'
+}
+console.log(man)
+
+// Classes
+
+class animal{
+    id: number
+    name: string
+    
+    constructor(id:number,name:string){
+        this.id = id
+        this.name =name
+    }
+}
+const dog = new animal(1, 'Spike')
+console.log(dog)
+
+// Generic
+
+function getArray<Type>(items:Type[]):Type[] {
+    return Array().concat(items)
+}
+let numArray = getArray<number>([1, 2, 3, 4, 5, 6]);
+let strArray = getArray<string>(['step','joe','doe']);
+
+numArray.splice(3)
+console.log(numArray)
